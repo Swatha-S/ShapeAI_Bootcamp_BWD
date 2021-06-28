f9e3c067b59ee2b4a8280ad19e527c6def8e87b0
Swatha-S <86589759+Swatha-S@users.noreply.github.com>
2021-06-28 12:16:08 +0530

    Create Info.jsx

---
+++ Info.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Info() {
+  return (
+    <div className="note">
+      <h1>Components of Computer System</h1>
+      <p> 1 Input Unit. A computer will only respond when a command is given to the device. ... 2 Output Unit. When we command a computer to perform a task, it reverts back for the action performed and gives us a result. 3 Memory Unit. ... 4 Control Unit. ... 5 Arithmetic & Logical Unit</p>
+    </div>
+  );
+}
+
+export default Info;
